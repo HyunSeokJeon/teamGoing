2721f93a669b8c6d79f8f34581ca56fa87f6c30c
HyunSeokJeon <sjunhs@gmail.com>
2021-06-24 16:58:32 +0900

    회원가입 오류안나게

--- goingFront/WebContent/frontResource/dist/js/joinCheck.js
+++ goingFront/WebContent/frontResource/dist/js/joinCheck.js
@@ -2,9 +2,35 @@
  * 
  */
 'use strict'
-let idtest;
-let passtest;
+let idtest = false;
+let idtest2 = false;
+let passtest = false;
+let passtest2 = false;
 
+
+
+let formPass = false;
+
+document.getElementById("joinForm").addEventListener("submit", (event)=>{
+	let nametest = isFilled("name");
+	let email1test = isFilled("email1");
+	let phonenumtest = isFilled("phonenum");
+	let addresscod1 = isFilled("addresscod1");
+	let address1 = isFilled("address1");
+	let address2 = isFilled("address2");
+	if (idtest && idtest2 
+		&& passtest && passtest2
+		&& nametest && email1test
+		&& phonenumtest && addresscod1
+		&& address1 && address2){
+
+		}else{
+			alert("입력값을 채워주세요!");
+			event.preventDefault();
+		}
+	
+
+});
 
 document.getElementById("inputId").addEventListener("keyup", () =>{
 	const idTag = document.getElementById("inputId");
@@ -30,17 +56,14 @@ document.getElementById("idCheck").addEventListener('click', function(){
 	
 });
 function idCheck(){
-	console.log(document.getElementById("inputId"))
 	let inputId = document.getElementById("inputId").value;
 	let httpRequest;
+
 	if (window.XMLHttpRequest) { // 모질라, 사파리, IE7+ ...
 		httpRequest = new XMLHttpRequest();
 	} else if (window.ActiveXObject) { // IE 6 이하
 		httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
 	}
-
-	
-	
 
 	httpRequest.open('POST', `/goingFront/idCheck.go?inputId=${inputId}`, true);
 
@@ -49,8 +72,10 @@ function idCheck(){
 			if(httpRequest.status === 200){
 				if(httpRequest.responseText === "true"){
 					alert("사용가능한 아이디입니다.");
+					idtest2 = true;
 				}else if(httpRequest.responseText === "false"){
 					alert("사용불가능한 아이디입니다.");
+					idtest2 = false;
 				}
 			}
 		}
@@ -89,16 +114,33 @@ function notNull(str){
 function passwordcheck(){
 	const password = document.getElementById("password");
 	const repassword = document.getElementById("repassword");
-	console.log(password);
-	console.log(repassword);
 	const repassConfirmTag = document.getElementById("repassConfirm");
 	if (password.value === repassword.value){
 		repassConfirmTag.style = "color:blue; font-size:11px;";
 		repassConfirmTag.innerText = '비밀번호가 일치합니다.';
+		passtest2=true;
 	}else{
 		repassConfirmTag.style = "color:red; font-size:11px;";
 		repassConfirmTag.innerText = "비밀번호가 일치하지 않습니다.";
+		passtest2=false;
 	}
 }
 
+function clickAction(tag, event1, event2){
+	tag.addEventListener(event1, goPopup);
+	tag.addEventListener(event2, goPopup);
+}
 
+clickAction(document.getElementById("addresscod1"), "click", "focus");
+clickAction(document.getElementById("address1"), "click", "focus");
+
+
+
+function isFilled(inputTagId){
+	let value = document.getElementById(inputTagId).value;
+	if(value.trim()!=="" && value.trim()!==null){
+		return true;
+	}else{
+		return false;
+	}
+}
