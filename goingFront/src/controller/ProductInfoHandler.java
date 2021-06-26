2f476b8603937b832e82bccbd7e42759b63a0d0e
HyunSeokJeon <sjunhs@gmail.com>
2021-06-26 18:28:01 +0900

    상품관련

--- goingFront/src/controller/ProductInfoHandler.java
+++ goingFront/src/controller/ProductInfoHandler.java
@@ -1,10 +1,14 @@
 package controller;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.ProductImageDao;
+import jdbc.connection.ConnectionProvider;
+import model.ProductImg;
 import model.Prolist;
 import mvc.command.CommandHandler;
 import service.ProlistService;
@@ -13,6 +17,7 @@ import service.User;
 public class ProductInfoHandler implements CommandHandler{
 
 	private ProlistService ps = new ProlistService();
+	private ProductImageDao pid = new ProductImageDao();
 	
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
@@ -41,8 +46,10 @@ public class ProductInfoHandler implements CommandHandler{
 			likeThisItem = ps.isLikeThisItem(productId, user.getId());
 		}
 		Prolist product = ps.getProduct(productId);
+		List<ProductImg> pImgList = pid.getImageList(ConnectionProvider.getConnection(), productId);
 		req.setAttribute("likeYN", likeThisItem);
 		req.setAttribute("product", product);
+		req.setAttribute("imgList", pImgList);
 		return "/WEB-INF/view/product/productDes.jsp";
 	}
 	
---
+++ goingFront/src/model/ProductImg.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.sql.Date;
+
+public class ProductImg {
+	private int productId;
+	private String pImg;
+	private String imgHead;
+	private Date regDate;
+	public int getProductId() {
+		return productId;
+	}
+	public String getpImg() {
+		return pImg;
+	}
+	public String getImgHead() {
+		return imgHead;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public ProductImg(int productId, String pImg, String imgHead, Date regDate) {
+		super();
+		this.productId = productId;
+		this.pImg = pImg;
+		this.imgHead = imgHead;
+		this.regDate = regDate;
+	}
+}
