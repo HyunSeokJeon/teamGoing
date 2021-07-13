e34d14c60ff2033abe58203ba7fa49df3894a0ac
HyunSeokJeon <sjunhs@gmail.com>
2021-07-13 14:38:44 +0900

    productType Controller

--- goingAdminSpring/src/main/java/org/going/productType/web/ProductTypeController.java
+++ goingAdminSpring/src/main/java/org/going/productType/web/ProductTypeController.java
@@ -1,5 +1,8 @@
 package org.going.productType.web;
 
+import javax.inject.Inject;
+
+import org.going.productType.service.ProductTypeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ProductTypeController {
 	
+	@Inject
+	private ProductTypeService service;
+	
 	@RequestMapping(value="/productType", method = RequestMethod.GET)
-	public String productTypeList(Model model) {
+	public String productTypeList(Model model) throws Exception{
+		model.addAttribute("list", service.listAll());
 		return "productType/productTypeMGMT";
 	}
 }
