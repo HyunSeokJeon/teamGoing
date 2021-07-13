187ee1e5e2112939ab3e3981e9f3c7bb8df83819
HyunSeokJeon <sjunhs@gmail.com>
2021-07-13 14:38:22 +0900

    productType VO

---
+++ goingAdminSpring/src/main/java/org/going/productType/domain/ProductTypeVO.java
@@ -0,0 +1,27 @@
+package org.going.productType.domain;
+
+public class ProductTypeVO {
+	private Integer productTypeId;
+	private String productType;
+	
+	public Integer getProductTypeId() {
+		return productTypeId;
+	}
+
+	public String getProductType() {
+		return productType;
+	}
+
+	public void setProductTypeId(Integer productTypeId) {
+		this.productTypeId = productTypeId;
+	}
+
+	public void setProductType(String productType) {
+		this.productType = productType;
+	}
+	
+	public String toString() {
+		return "ProductTypeVO [productTypeId = " + productTypeId + ", productType = " + productType;
+	}
+	
+}
